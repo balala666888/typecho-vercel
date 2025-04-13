e1e14e0a64a794310d4e9c058d53db0a5fd328e7
hua <116322279+balala666888@users.noreply.github.com>
2025-04-13 09:37:54 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -20,12 +20,12 @@ require_once __TYPECHO_ROOT_DIR__ . '/var/Typecho/Common.php';
 // config db
 $db = new \Typecho\Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => 'sql.freedb.tech',
-  'port' => 3306,
-  'user' => 'freedb_hua8013',
-  'password' => '&EJKqs*Js#3CWYe',
+  'host' => 'mysql2.sqlpub.com',
+  'port' => 3307,
+  'user' => 'aa992040',
+  'password' => 'MKocVBsii6h5TAAI',
   'charset' => 'utf8mb4',
-  'database' => 'freedb_992040',
+  'database' => 'aa992040',
   'engine' => 'InnoDB',
 ), \Typecho\Db::READ | \Typecho\Db::WRITE);
 \Typecho\Db::set($db);
